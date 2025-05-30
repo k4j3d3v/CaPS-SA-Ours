692a3bde3d89e37c457649c5fdebdd43691bf60a
jamshed <jamshed@umd.edu>
2025-05-30 15:50:02 -0400

    Fix bug at tail of text

--- src/Genomic_Text.cpp
+++ src/Genomic_Text.cpp
@@ -12,7 +12,7 @@ namespace CaPS_SA
 Genomic_Text::Genomic_Text(const char* const T, const std::size_t n):
       n_(n)
     , pack_sz((n_ + 3) / 4)
-    , B(pack_sz + 8)    // Why +8?
+    , B(pack_sz + 1)    // +1 byte to facilitate one fell swoop loading of the last 31 bytes of the bit-packed text.
 {
     const auto code = [](char ch)
     {
@@ -38,7 +38,7 @@ Genomic_Text::Genomic_Text(const char* const T, const std::size_t n):
     {
         B[n_ / 4] = 0;
         for(std::size_t i = 0; i < (n_ % 4); ++i)
-            B[n_ / 4] = (code(T[(n_ / 4) * 4 + i]) << (2 * i));
+            B[n_ / 4] |= (code(T[(n_ / 4) * 4 + i]) << (2 * i));
     }
 }
 
