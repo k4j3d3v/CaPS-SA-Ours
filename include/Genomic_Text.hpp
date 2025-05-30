6863c35fb7007edfec0262de89cb6fe4dcb2b099
jamshed <jamshed@umd.edu>
2025-05-30 15:59:57 -0400

    Add base accessor

--- include/Genomic_Text.hpp
+++ include/Genomic_Text.hpp
@@ -6,14 +6,15 @@
 
 #include <cstdint>
 #include <cstddef>
-
 #include <vector>
+#include <cassert>
 
 
 namespace CaPS_SA
 {
 
-// Class to to represent genomic texts in a 2-bit-packed manner.
+// Class to to represent genomic texts in a 2-bit-packed manner. The bit-packed
+// representation is considered to run right-to-left.
 class Genomic_Text
 {
     const std::size_t n_;   // Length of the input text.
@@ -26,7 +27,26 @@ public:
     // Constructs a 2-bit-packed representation of the genomic text `T` of
     // length `n`.
     Genomic_Text(const char* T, std::size_t n);
+
+    Genomic_Text(const Genomic_Text&)  = delete;
+    Genomic_Text(Genomic_Text&&) = delete;
+    Genomic_Text& operator=(const Genomic_Text&) = delete;
+    Genomic_Text& operator=(Genomic_Text&&) = delete;
+
+    // Returns the code of the nucleobase at index `idx` of the original text.
+    uint8_t base_at(std::size_t idx) const;
 };
+
+
+inline uint8_t Genomic_Text::base_at(const std::size_t idx) const
+{
+    assert(idx < n_);
+
+    const auto byte_idx = idx >> 2;
+    const auto bit_idx = (idx & 0b11) * 2;
+
+    return  (B[byte_idx] & (0b11 << bit_idx)) >> bit_idx;
+}
 
 }
 
