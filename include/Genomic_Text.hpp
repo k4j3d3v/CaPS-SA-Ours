5eb57cecfaf6037efc8165d7e7722e5f5888e87d
jamshed <jamshed@umd.edu>
2025-06-20 16:58:31 -0400

    Compile conditionally on platforms

--- include/Genomic_Text.hpp
+++ include/Genomic_Text.hpp
@@ -4,13 +4,17 @@
 
 
 
-#include "x86/avx2.h"
-
 #include <cstdint>
 #include <cstddef>
 #include <cstring>
 #include <vector>
 #include <cassert>
+
+#ifdef NATIVE_SIMD
+#include <immintrin.h>
+#else
+#include "x86/avx2.h"
+#endif
 
 
 namespace CaPS_SA
@@ -25,15 +29,24 @@ class Genomic_Text
     const std::size_t pack_sz;  // Size of the bit-packed text.
     std::vector<uint8_t> B; // The bit-packed text.
 
+#ifdef NATIVE_SIMD
+#ifndef USE_AVX_512
+    // Returns the 124-nucleobase block (31 bytes) from onward the `i`'th
+    // nucleobase, in 256-bits little-endian. No guarantees are provided for
+    // the highest byte.
+    __m256i load(std::size_t i) const;
+#else
+    // Returns the 252-nucleobase block (63 bytes) from onward the `i`'th
+    // nucleobase, in 256-bits little-endian. No guarantees are provided for
+    // the highest byte.
+    __m512i load(std::size_t i) const;
+#endif
+#else
     // Returns the 124-nucleobase block (31 bytes) from onward the `i`'th
     // nucleobase, in 256-bits little-endian. No guarantees are provided for
     // the highest byte.
     simde__m256i load(std::size_t i) const;
-
-    // Returns the 252-nucleobase block (63 bytes) from onward the `i`'th
-    // nucleobase, in 256-bits little-endian. No guarantees are provided for
-    // the highest byte.
-    __m512i load_512(std::size_t i) const;
+#endif
 
     // Returns the 29-nucleobase block (8 bytes) from onward the `i`'th
     // nucleobase, in 64-bits little-endian. The highest 6-bits are zeroed.
@@ -82,29 +95,33 @@ inline uint8_t Genomic_Text::operator[](const std::size_t idx) const
 }
 
 
-inline simde__m256i Genomic_Text::load(const std::size_t i) const
+#ifdef NATIVE_SIMD
+#ifndef USE_AVX_512
+
+inline __m256i Genomic_Text::load(const std::size_t i) const
 {
     assert(i + 124 <= n_);
 
     const auto base = i / 4;    // Base word's index.
-    const auto blk = simde_mm256_loadu_si256(reinterpret_cast<const simde__m256i*>(B.data() + base)); // 256-bits block from the base word.
+    const auto blk = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(B.data() + base)); // 256-bits block from the base word.
 
     const auto unwanted_trail = i & 3;  // Number of unwanted bases (2-bits) trailing in the base word.
     if(!unwanted_trail)
         return blk;
 
-    const auto to_clear_trail = simde_mm256_set1_epi64x(unwanted_trail * 2); // Number of trailing bits to clear from each word.
-    const auto cleared = simde_mm256_srlv_epi64(blk, to_clear_trail);    // Trailing bits cleared from each 64-bit word.
-    const auto r_shifted = simde_mm256_permute4x64_epi64(blk, 0b00'11'10'01);    // Words right-shifted by 1 word. The top word is don't-care.
-    const auto to_clear_lead = simde_mm256_set1_epi64x((32 - unwanted_trail) * 2);    // Number of leading bits to clear from each word of the right-shifted block.
-    const auto lost_bits = simde_mm256_sllv_epi64(r_shifted, to_clear_lead); // Bits lost due to the inability of whole register-wise right-shift during clearance of unwanted-bits.
-    const auto restored = simde_mm256_or_si256(cleared, lost_bits);  // Restored lost trailing bits from words 1, 2, and 3.
+    const auto to_clear_trail = _mm256_set1_epi64x(unwanted_trail * 2); // Number of trailing bits to clear from each word.
+    const auto cleared = _mm256_srlv_epi64(blk, to_clear_trail);    // Trailing bits cleared from each 64-bit word.
+    const auto r_shifted = _mm256_permute4x64_epi64(blk, 0b00'11'10'01);    // Words right-shifted by 1 word. The top word is don't-care.
+    const auto to_clear_lead = _mm256_set1_epi64x((32 - unwanted_trail) * 2);    // Number of leading bits to clear from each word of the right-shifted block.
+    const auto lost_bits = _mm256_sllv_epi64(r_shifted, to_clear_lead); // Bits lost due to the inability of whole register-wise right-shift during clearance of unwanted-bits.
+    const auto restored = _mm256_or_si256(cleared, lost_bits);  // Restored lost trailing bits from words 1, 2, and 3.
 
     return restored;
 }
 
+#else
 
-inline __m512i Genomic_Text::load_512(const std::size_t i) const
+inline __m512i Genomic_Text::load(const std::size_t i) const
 {
     assert(i + 252 <= n_);
 
@@ -127,6 +144,32 @@ inline __m512i Genomic_Text::load_512(const std::size_t i) const
     return restored;
 }
 
+#endif
+#else
+
+inline simde__m256i Genomic_Text::load(const std::size_t i) const
+{
+    assert(i + 124 <= n_);
+
+    const auto base = i / 4;    // Base word's index.
+    const auto blk = simde_mm256_loadu_si256(reinterpret_cast<const simde__m256i*>(B.data() + base)); // 256-bits block from the base word.
+
+    const auto unwanted_trail = i & 3;  // Number of unwanted bases (2-bits) trailing in the base word.
+    if(!unwanted_trail)
+        return blk;
+
+    const auto to_clear_trail = simde_mm256_set1_epi64x(unwanted_trail * 2); // Number of trailing bits to clear from each word.
+    const auto cleared = simde_mm256_srlv_epi64(blk, to_clear_trail);    // Trailing bits cleared from each 64-bit word.
+    const auto r_shifted = simde_mm256_permute4x64_epi64(blk, 0b00'11'10'01);    // Words right-shifted by 1 word. The top word is don't-care.
+    const auto to_clear_lead = simde_mm256_set1_epi64x((32 - unwanted_trail) * 2);    // Number of leading bits to clear from each word of the right-shifted block.
+    const auto lost_bits = simde_mm256_sllv_epi64(r_shifted, to_clear_lead); // Bits lost due to the inability of whole register-wise right-shift during clearance of unwanted-bits.
+    const auto restored = simde_mm256_or_si256(cleared, lost_bits);  // Restored lost trailing bits from words 1, 2, and 3.
+
+    return restored;
+}
+
+#endif
+
 
 inline uint64_t Genomic_Text::load_word(const std::size_t i) const
 {
@@ -148,11 +191,12 @@ inline std::size_t Genomic_Text::LCP_unrolled(const std::size_t x, const std::si
         return 0;
     else
     {
-#ifndef USE_AVX_512
         const auto X = load(x);
         const auto Y = load(y);
 
-        const auto eq_mask = simde_mm256_movemask_epi8(simde_mm256_cmpeq_epi8(X, Y));
+#ifdef NATIVE_SIMD
+#ifndef USE_AVX_512
+        const auto eq_mask = _mm256_movemask_epi8(_mm256_cmpeq_epi8(X, Y));
         const auto neq_mask = ~eq_mask & 0x7FFF'FFFF;   // Top byte is degenerate in a block.
         if(neq_mask)
         {
@@ -166,10 +210,8 @@ inline std::size_t Genomic_Text::LCP_unrolled(const std::size_t x, const std::si
         }
 
         return 124 + LCP_unrolled<N - 1>(x + 124, y + 124);
-#else
-        const auto X = load_512(x);
-        const auto Y = load_512(y);
 
+#else
         const auto eq_mask = _mm512_cmpeq_epi8_mask(X, Y);
         const auto neq_mask = ~eq_mask & 0x7FFF'FFFF'FFFF'FFFFull;  // Top byte is degenerate in a block.
         if(neq_mask)
@@ -184,6 +226,24 @@ inline std::size_t Genomic_Text::LCP_unrolled(const std::size_t x, const std::si
         }
 
         return 252 + LCP_unrolled<N - 1>(x + 252, y + 252);
+
+#endif
+#else
+        const auto eq_mask = simde_mm256_movemask_epi8(simde_mm256_cmpeq_epi8(X, Y));
+        const auto neq_mask = ~eq_mask & 0x7FFF'FFFF;   // Top byte is degenerate in a block.
+        if(neq_mask)
+        {
+            auto const X_b = reinterpret_cast<const unsigned char*>(&X);
+            auto const Y_b = reinterpret_cast<const unsigned char*>(&Y);
+            const auto bytes_eq = __builtin_ctz(neq_mask);
+            assert(X_b[bytes_eq] != Y_b[bytes_eq]);
+
+            const auto bits_eq = (bytes_eq << 3) + (__builtin_ctz(X_b[bytes_eq] ^ Y_b[bytes_eq]));
+            return bits_eq >> 1;
+        }
+
+        return 124 + LCP_unrolled<N - 1>(x + 124, y + 124);
+
 #endif
     }
 }
