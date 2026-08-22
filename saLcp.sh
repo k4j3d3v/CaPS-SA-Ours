41cf9522ef14181fab222c4147fe1013075daf04
Giovanni Manzini <giovanni.manzini@unipi.it>
2026-08-22 09:46:26 +0000

    .stat deleted in saLcp

--- saLcp.sh
+++ saLcp.sh
@@ -35,5 +35,5 @@ do
   cat $f.stat
   # compute sha1sum and delete (after the initial sha1 computation)
   sha1sum  $f $f.lcp $f.sa >> $sha1file 
-  rm -f $f.sa $f.lcp
+  rm -f $f.sa $f.lcp $f.stat
 done
